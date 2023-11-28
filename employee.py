d60671fb1f8566f3252ca4a3fd5b055ef4a0e847
Brian <probrian89@gmail.com>
2023-11-29 01:21:44 +0800

    employees

--- employee.py
+++ employee.py
@@ -11,6 +11,94 @@ class Employee:
     def __str__(self):
         return self.name
 
+class SalaryEmployee(Employee):
+    def __init__(self, name, monthly_salary):
+        super().__init__(name)
+        self.monthly_salary = monthly_salary
+
+    def get_pay(self):
+        return self.monthly_salary
+
+    def __str__(self):
+        return f"{self.name} works on a monthly salary of {self.monthly_salary}. Their total pay is {self.get_pay()}."
+
+
+class HourlyEmployee(Employee):
+    def __init__(self, name, hours_worked, hourly_rate):
+        super().__init__(name)
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+
+    def get_pay(self):
+        return self.hours_worked * self.hourly_rate
+
+    def __str__(self):
+        return f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour. Their total pay is {self.get_pay()}."
+
+
+class SalaryEmployeeWithBonus(SalaryEmployee):
+    def __init__(self, name, monthly_salary, bonus_commission):
+        super().__init__(name, monthly_salary)
+        self.bonus_commission = bonus_commission
+
+    def get_pay(self):
+        return super().get_pay() + self.bonus_commission
+
+    def __str__(self):
+        salary_info = f"{self.name} works on a monthly salary of {self.monthly_salary}"
+        bonus_info = f"and receives a bonus commission for {self.bonus_commission}."
+        total_pay_info = f"Their total pay is {self.get_pay()}.$
+        return f"{salary_info} {bonus_info} {total_pay_info}"
+
+
+class HourlyEmployeeWithBonus(HourlyEmployee):
+    def __init__(self, name, hours_worked, hourly_rate, bonus_commission):
+        super().__init__(name, hours_worked, hourly_rate)
+        self.bonus_commission = bonus_commission
+
+    def get_pay(self):
+        return super().get_pay() + self.bonus_commission
+
+    def __str__(self):
+        hourly_info = f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour"
+        bonus_info = f"and receives a bonus commission for {self.bonus_commission}."
+        total_pay_info = f"Their total pay is {self.get_pay()}."
+        return f"{hourly_info} {bonus_info} {total_pay_info}"
+
+
+
+class SalaryEmployeeWithContractCommission(SalaryEmployee):
+    def __init__(self, name, monthly_salary, num_commissions, commission_rate):
+        super().__init__(name, monthly_salary)
+        self.num_commissions = num_commissions
+        self.commission_rate = commission_rate
+
+    def get_pay(self):
+        commission_pay = self.num_commissions * self.commission_rate
+        return super().get_pay() + commission_pay
+
+    def __str__(self):
+        salary_info = f"{self.name} works on a monthly salary of {self.monthly_salary}"
+        commission_info = f"and receives a commission for {self.num_commissions} contract(s) at {self.commission_rate}/contract."
+        total_pay_info = f"Their total pay is {self.get_pay()}.$"
+        return f"{salary_info} {commission_info} {total_pay_info}"
+
+class HourlyEmployeeWithContractCommission(HourlyEmployee):
+    def __init__(self, name, hours_worked, hourly_rate, num_commissions, commission_rate):
+        super().__init__(name, hours_worked, hourly_rate)
+        self.num_commissions = num_commissions
+        self.commission_rate = commission_rate
+
+    def get_pay(self):
+        contract_pay = super().get_pay()
+        commission_pay = self.num_commissions * self.commission_rate
+        return contract_pay + commission_pay
+
+    def __str__(self):
+        hourly_info = f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour"
+        commission_info = f"and receives a commission of {self.num_commissions} contract(s) at {self.commission_rate}/contract."
+        total_pay_info = f"Their total pay is {self.get_pay()}."
+        return f"{hourly_info} {commission_info} {total_pay_info}"
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie')
