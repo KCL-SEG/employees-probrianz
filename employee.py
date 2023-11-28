7782916526df9bf67c5a5089cd8e23b9ab3d3f00
Brian <probrian89@gmail.com>
2023-11-29 01:29:39 +0800

    employees

--- employee.py
+++ employee.py
@@ -47,7 +47,7 @@ class SalaryEmployeeWithBonus(SalaryEmployee):
     def __str__(self):
         salary_info = f"{self.name} works on a monthly salary of {self.monthly_salary}"
         bonus_info = f"and receives a bonus commission for {self.bonus_commission}."
-        total_pay_info = f"Their total pay is {self.get_pay()}.$
+        total_pay_info = f"Their total pay is {self.get_pay()}."
         return f"{salary_info} {bonus_info} {total_pay_info}"
 
 
